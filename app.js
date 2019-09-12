1d6a50724b9e968de12949f08610f26850ec653d
aofakore <adekolafakorede@gmail.com>
2019-09-11 21:53:37 -0400

    run hand detection and render predictions

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const audio = document.querySelector("#audio");
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 let model;
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.79 // confidence threshold for predictions.
+};
 
 handTrack.startVideo(video).then(status => {
   if (status) {
@@ -17,10 +24,18 @@ handTrack.startVideo(video).then(status => {
       { video: {} },
       stream => {
         video.srcObject = stream;
+        setInterval(runDetection, 10);
       },
       err => console.log(err)
     );
   }
 });
 
-handTrack.load().then(loadedModel => (model = loadedModel));
+const runDetection = () => {
+  model.detect(video).then(predictions => {
+    console.log(predictions);
+    model.renderPredictions(predictions, canvas, context, video);
+  });
+};
+
+handTrack.load(modelParams).then(loadedModel => (model = loadedModel));
